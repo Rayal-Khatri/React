ee5b4cbf9e356fc44fef26f5252a858b8d1c4115
Rayal Khatri <kcrayalgw@gmail.com>
2023-04-19 21:55:02 +0545

    Configuring page content form other js files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import {TodoItems} from "./MyComponents/TodoItems";
+import {Footer} from "./MyComponents/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Myclasses">
+      <Header/>
+      <TodoItems/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/MyComponents/TodoItems.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TodoItems = () => {
+  return (
+    <div>Todos is present here</div>
+  )
+}
