d0a47acdb5716807a2f176bf1550b7d471e8fea6
Rayal kc <kcrayalgw@gmail.com>
2023-04-25 22:55:33 +0545

    learning...

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {Footer} from "./MyComponents/Footer";
 function App() {
   return (
     <div className="Myclasses">
-      <Header title="Hello world"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+      <Header  searchBar={false}/>
       <TodoItems/>
       <Footer/>
     </div>
